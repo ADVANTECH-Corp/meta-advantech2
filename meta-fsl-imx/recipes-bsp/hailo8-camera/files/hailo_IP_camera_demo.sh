dccf6f63bf762d799a784cd14057b7cdf61a91e5
kuihong.xin <kuihong.xin@advantech.com.cn>
2023-11-17 16:15:11 +0800

    [5710] modified run path

--- meta-fsl-imx/recipes-bsp/hailo8-camera/files/hailo_IP_camera_demo.sh
+++ meta-fsl-imx/recipes-bsp/hailo8-camera/files/hailo_IP_camera_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#/home/root/apps/advantech_0529/1or2_cam_detection_facial_landmark_both.sh
-/home/root/apps/advantech_0529/2or4_cam_detection_facial_landmark_independence.sh --num-of-sources 2
+#/home/root/apps/ip_camera_demo/1or2_cam_detection_facial_landmark_both.sh
+/home/root/apps/ip_camera_demo/2or4_cam_detection_facial_landmark_independence.sh --num-of-sources 2
 
