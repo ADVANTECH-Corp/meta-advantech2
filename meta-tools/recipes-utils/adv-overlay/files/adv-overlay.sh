37f1dc27f3b2f509dbfd14cff2c751af2838029c
kuihong.xin <kuihong.xin@advantech.com.cn>
2024-04-28 14:08:34 +0800

    [RSB-3730] support adv-overlay fix wifi modeprobe issue

---
+++ meta-tools/recipes-utils/adv-overlay/files/adv-overlay.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+OVERLAY_FLAG="/tools/adv-overlay/ADV_OVERLAY"
+
+if [ -e "${OVERLAY_FLAG}" ]
+then
+	echo "adv-overlay.sh start"
+##################START YOUR WORK#######################
+
+# fix wifi modeproe issue ,(rm -rf /lib/moduels/xxx/extra)
+	M_NAME=`uname -a | awk {print'$3'}`
+	[ -e /lib/modules/$M_NAME ] && echo "/lib/modules/$M_NAME File exists"
+	[ -e /lib/modules/$M_NAME/extra ]  && echo "/lib/modules/$M_NAME/extra File exists , start to remove"
+	[ -e /lib/modules/$M_NAME/extra ]  && rm -rf /lib/modules/$M_NAME/extra
+	sync
+
+
+###################END YOUR WORK########################
+	sync
+	rm "${OVERLAY_FLAG}"
+	echo "adv-overlay.sh end"
+fi
+
+#systemctl disable adv-overlay.service
+
+sync
